0e4d150776ead647d6ad237ebd604cc4251ff4c9
AliNad99 <alexander.nadler@gmx.de>
2021-02-18 21:06:51 +0100

    Projekt 4 - accordion - finished

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
-import data from './data';
+import data from './data.js';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+
+  const [questions, setQuestion] = useState(data);
+  const [visible, setVisible] = useState(true)
+
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions And Answers About Login</h3>
+        <section>
+            {questions.map(question => 
+               <SingleQuestion key={question.id} {...question} setVisible={setVisible}/>
+            )}
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info, d}) => {
+
+  const [visible, setVisible] = useState(false)
+
+  function toggleVisible(){
+    if(visible){
+      setVisible(false)
+    } else {
+      setVisible(true)
+    }
+  }
+
+  return (
+    <div className="question">
+
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => toggleVisible()}>
+          {
+            visible
+              ?<AiOutlineMinus />
+              :<AiOutlinePlus />
+          }
+        </button>
+      </header>
+
+      <p>
+        {visible
+        ?info
+        :info.substr(0, 0)
+        }
+      </p>
+
+    </div>
+    )
 };
 
 export default Question;
