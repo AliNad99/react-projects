ff65fab8c6701251be923a325b3cbd74542b890c
AliNad99 <alexander.nadler@gmx.de>
2021-02-22 22:25:05 +0100

    Prjoekt 9 - color generator - finished

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index}) => {
+
+  
+  const [alert, setAlert] = useState(false);
+
+  const bcg = `rgb(${rgb.toString()})`;
+  const hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
+
+  return (
+    <article className={`color ${index > 10 && "color-light"}`} style={{backgroundColor: bcg}}>
+      <p className="percent-value">{weight}% </p>
+      <p className="color-value">{bcg}</p>
+      <p className="color-value">{hex}</p>
+    </article>
+  )
 }
 
 export default SingleColor
